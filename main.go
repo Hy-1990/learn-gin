4db5cda20f57d3a35f26c6d0964806bc1cb40c8a
huyi <3614322595@qq.com>
2022-06-09 20:52:11 +0800

    跨域配置、全局捕获

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"learn-gin/app/router"
 	"learn-gin/config/log"
 	"learn-gin/config/toml"
+	"net/http"
+	"time"
 )
 
 func main() {
@@ -13,7 +15,19 @@ func main() {
 	log.Logger.Info("config", log.Any("config", toml.GetConfig()))
 
 	r := gin.Default()
+	r.Use(router.Cors())
+	r.Use(router.Recovery)
 	router.InitRouter(r)
 
-	r.Run(":8080")
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        r,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	err := s.ListenAndServe()
+	if nil != err {
+		log.Logger.Error("server error", log.Any("serverError", err))
+	}
 }
